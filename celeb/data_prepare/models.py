336c7b508a60981e170a018074346ab13269e0d2
4repaxxx <4repaxxx@unn.com>
2025-05-20 19:45:08 +0300

    Add frame processor

--- celeb/data_prepare/models.py
+++ celeb/data_prepare/models.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
-from tensorflow.keras.models import Model
+from keras.models import Model, Sequential
+from tensorflow.keras.regularizers import l2
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Input
 from tensorflow.keras.applications import Xception, EfficientNetB0
 from tensorflow.keras.optimizers import Adam
@@ -26,11 +27,12 @@ class ModelType(Enum):
     CUSTOM = "custom_cnn"
 
 class DataLoader:
-    def __init__(self, image_archive_path, model_type=ModelType.XCEPTION, augmentation=False):
+    def __init__(self, image_archive_path, output_dir, model_type=ModelType.XCEPTION, augmentation=False):
         self._image_archive_path = image_archive_path
         self._model_type = model_type
         self._augmentation = augmentation
         self._preprocess_func = tf.keras.applications.xception.preprocess_input
+        self._output_dir = output_dir
         self._postfix = 'x'
         self._image_size = (299, 299)
         if self._model_type == ModelType.EFFICIENTNET:
@@ -39,16 +41,15 @@ class DataLoader:
             self._image_size = (224, 224)
             
 
-    def load(self, output_dir):
-        return self._init_data(os.path.join(output_dir, self._postfix))
+    def load(self):
+        return self._init_data(os.path.join(self._output_dir, self._postfix))
 
-    def load_genrators(self, output_dir):
-        train_dir, val_dir, test_dir = self._init_data(os.path.join(output_dir, self._postfix))
+    def load_genrators(self):
+        train_dir, val_dir, test_dir = self._init_data(os.path.join(self._output_dir, self._postfix))
         print(train_dir, val_dir, test_dir)
         return self._create_data_generators(train_dir, val_dir, test_dir)
 
     def _init_data(self, output_dir):
-        
         fake_images_path, real_images_path = extract_zip_with_cleanup(self._image_archive_path)
         print(fake_images_path, real_images_path)
         train_dir, val_dir, test_dir = prepare_and_save_data(
@@ -136,6 +137,14 @@ class DataLoader:
         
         return train_gen, val_gen, test_gen
 
+class XDataLoader(DataLoader):
+    def __init__(self, image_archive_path, output_dir="data/dataset"):
+        super().__init__(image_archive_path, output_dir, ModelType.XCEPTION, False)
+
+class NetDataLoader(DataLoader):
+    def __init__(self, image_archive_path, output_dir="data/dataset"):
+        super().__init__(image_archive_path, output_dir, ModelType.EFFICIENTNET, False)
+
 class TrainedModel:
     def __init__(self, model_type, input_shape):
         self._model_type = model_type
@@ -156,45 +165,6 @@ class TrainedModel:
                 input_shape=self._input_shape
             )
         return base_model
-    
-
-class FeatureExtractorModel(TrainedModel):
-    def __init__(self, model_type=ModelType.XCEPTION, input_shape=(299, 299, 3), fine_tune=False):
-        super().__init__(model_type, input_shape, False)
-        self._model = self._build_model()
-
-    def _build_model(self):
-        inputs = Input(shape=self._input_shape)
-        
-        x = self._base_model(inputs)
-        x = GlobalAveragePooling2D()(x)
-        x = BatchNormalization()(x)
-        x = Dense(256, activation='relu')(x)
-
-        model = Model(inputs=inputs, outputs=x)
-        model.compile(
-            optimizer=Adam(),
-            loss="binary_crossentropy",
-            metrics=[
-                tf.keras.metrics.Precision(name="precision"),
-                tf.keras.metrics.Recall(name="recall"),
-                tf.keras.metrics.AUC(name="auc"),
-            ]
-        )
-        return model
-
-    def predict(self, frames):
-        """Extract features from the video frames"""
-        if len(frames.shape) == 3:
-            frames = np.expand_dims(frames, axis=0)
-
-        if self._model_type == ModelType.EFFICIENTNET:
-            processed_frames = tf.keras.applications.efficientnet.preprocess_input(frames)
-        else:
-            processed_frames = tf.keras.applications.xception.preprocess_input(frames)
-
-        features = self._model.predict(processed_frames)
-        return features
         
         
 class FineTunedModel(TrainedModel):
@@ -305,8 +275,14 @@ class FineTunedModel(TrainedModel):
         
         return self._model, full_history
 
-    def load_trained_model():
-        return tf.keras.models.load_model(self._models_dir)
+    def predict(self, X_test):
+        return self._model.predict(X_test)
+
+    def evaluate(self, X_test, y_test):
+        return self._model.evaluate(X_test, y_test)
+
+    def load_trained_model(self):
+        self._model = tf.keras.models.load_model(os.path.join(self._models_dir, self._funetune_model_name))
 
 class XFineTunedModel(FineTunedModel):
     def __init__(self, models_dir):
@@ -324,5 +300,133 @@ class NetFineTunedModel(FineTunedModel):
         net_models_dir = os.path.join(models_dir, 'net')
         input_shape=(224, 224, 3)
         super().__init__(net_models_dir, funetune_model_name, model_type, input_shape)
+
+
+class FeatureExtractorModel(TrainedModel):
+    def __init__(self, model_type=ModelType.XCEPTION, input_shape=(299, 299, 3)):
+        super().__init__(model_type, input_shape)
+        self._model = self._build_model()
+
+    def _build_model(self):
+        inputs = Input(shape=self._input_shape)
         
-    
+        x = self._base_model(inputs)
+        x = GlobalAveragePooling2D()(x)
+        x = BatchNormalization()(x)
+        x = Dense(256, activation='relu')(x)
+
+        model = Model(inputs=inputs, outputs=x)
+        model.compile(
+            optimizer=Adam(),
+            loss="binary_crossentropy",
+            metrics=[
+                tf.keras.metrics.Precision(name="precision"),
+                tf.keras.metrics.Recall(name="recall"),
+                tf.keras.metrics.AUC(name="auc"),
+            ]
+        )
+        return model
+
+    def predict(self, frames):
+        """Extract features from the video frames"""
+        # if len(frames.shape) == 3:
+        #     frames = np.expand_dims(frames, axis=0)
+        # if self._model_type == ModelType.EFFICIENTNET:
+        #     processed_frames = tf.keras.applications.efficientnet.preprocess_input(frames)
+        # else:
+        #     processed_frames = tf.keras.applications.xception.preprocess_input(frames)
+
+        features = self._model.predict(frames)
+        return features
+
+class XFeatureExtractorModel(FeatureExtractorModel):
+    def __init__(self):
+        super().__init__(ModelType.XCEPTION, (299, 299, 3))
+        
+
+class CustomModel:
+    def __init__(self, ex_feature_model, models_dir, model_name, learning_rate, input_dim=256, batch_size=32, epoch=10):
+        self._models_dir = models_dir
+        self._model_name = model_name
+        self._ex_feature_model = ex_feature_model
+        self._batch_size = batch_size
+        self._epoch = epoch
+        self._learning_rate = learning_rate
+        self._model = self._build_classifier(input_dim)
+
+    def _build_classifier(self, input_dim):
+        """Build binary classifier model"""
+        model = Sequential([
+            Dense(256, activation='relu', input_dim=input_dim, kernel_regularizer=l2(0.01)),
+            BatchNormalization(),
+            Dropout(0.5),
+            Dense(128, activation='relu', kernel_regularizer=l2(0.01)),
+            BatchNormalization(),
+            Dropout(0.5),
+            Dense(64, activation='relu', kernel_regularizer=l2(0.01)),
+            BatchNormalization(),
+            Dense(1, activation='sigmoid')
+        ])
+
+        model.compile(
+            optimizer=tf.keras.optimizers.Adam(self._learning_rate),
+            loss='binary_crossentropy',
+            metrics=['accuracy', tf.keras.metrics.AUC()]
+        )
+
+        return model
+
+    def fit(self, X_gen, v_gen):
+        """Train the classifier"""
+        callbacks = [
+            tf.keras.callbacks.EarlyStopping(patience=10, restore_best_weights=True),
+            tf.keras.callbacks.ModelCheckpoint(
+                f"{self._models_dir}/{self._model_name}",
+                save_best_only=True,
+                monitor='val_accuracy'
+            ),
+            tf.keras.callbacks.TensorBoard(log_dir='logs')
+        ]
+
+        history = self._model.fit(
+            X_gen, 
+            validation_data=v_gen,
+            epochs=self._epoch,
+            batch_size=self._batch_size,
+            callbacks=callbacks
+        )
+
+        return history
+
+    def evaluate(self, X_test, y_test):
+        """Evaluate model performance"""
+        return self.model.evaluate(X_test, y_test)
+
+    def predict(self, X):
+        """Make predictions"""
+        return self.model.predict(X)
+
+class XBaseAndCustomModel(CustomModel):
+    def __init__(self, models_dir, input_dim=256, batch_size=32, epoch=10):
+        ex_feature_model = XFeatureExtractorModel()
+        model_name='xbase_custom_model.h5'
+        learning_rate=1e-4
+        super().__init__(ex_feature_model, models_dir, model_name, learning_rate)
+
+class FrameProcessor:
+    def __init__(self, ex_feature_model, custom_model, data_loader):
+        self._ex_feature_model = ex_feature_model
+        self._custom_model = custom_model
+        self._data_loader = data_loader
+
+    def process(self):
+        train, val, test = data_loader.load_genrators()
+        train_frame_features = np.mean(self._ex_feature_model.predict(train), axis=0)
+        val_frame_features = np.mean(self._ex_feature_model.predict(val), axis=0)
+        test_frame_features = np.mean(self._ex_feature_model.predict(test), axis=0)
+        
+        history = self._custom_model.fit(train_frame_features, val_frame_features)
+        loss, acc, auc = self._custom_model.evaluate(test_frame_features, y_test)
+
+        return history, loss, acc, auc 
+        
